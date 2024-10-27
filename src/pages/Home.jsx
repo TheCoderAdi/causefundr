ae5c54dc6051e348707d13d66d600cb8d1d7536d
Aditya <adityasahoo652@gmail.com>
2024-10-27 16:43:32 +0530

    chore: Update landing page banner text to "CauseFundr"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ const Hero = ({ createCampaign }) => {
           <div className="flex flex-col items-center justify-between xl:flex-row">
             <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
               <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-5xl sm:leading-none">
-                Crowd Fundr <br className="hidden md:block" />
+                CauseFundr <br className="hidden md:block" />
                 Crowd Funding DApp
               </h2>
               <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
